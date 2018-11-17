8b99f3ec1c6ade629b0de33ca5ef071e2f685924
Naren Dikkala <nrdikkala1@gmail.com>
2018-11-16 19:38:26 -0500

    Update siftfunc
    Values are hardcoded to operate within folder with the npy file for Caltech

--- preprocessing/siftfunc.py
+++ preprocessing/siftfunc.py
@@ -2,9 +2,21 @@ import cv2
 import numpy as np
 from getSift import getSift
 from tempfile import TemporaryFile
+import os
+# import 'input_paths.npy' as inputArr
+# import 'output_paths.npy' as outputArr
 
-def siftFunc(input, output):
-    outfile = TemporaryFile()
-    img = cv2.imread(input)
+inArr = np.load('input_paths.npy')
+outArr = np.load('output_paths.npy')
+for i in range (0, len(inArr)):
+    curIn = inArr[i]
+    curIn = os.getcwd()+'/'+curIn
+    img = cv2.imread(curIn)
     siftArr = getSift(img)
-    np.save('output.npy', siftArr)
+    curOut = outArr[i]
+    curOut = os.getcwd()+'/'+curOut
+    np.save(curOut, siftArr)
+# outfile = TemporaryFile()
+# img = cv2.imread(input)
+# siftArr = getSift(img)
+# np.save('output.npy', siftArr)
